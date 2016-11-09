22486cb9c26619478e4f8729292a9c856905127a
simuloid <simuloid-git@gmail.com>
2016-11-09 14:39:29 -0500

    Added clone constructor.  Added methods to update internal state based on moving forward or turning.

--- src/main/java/europasw/neonav/Pose.java
+++ src/main/java/europasw/neonav/Pose.java
@@ -27,4 +27,52 @@ public class Pose {
         this.y = y;
         this.heading = heading;
     }
+    
+    public Pose(Pose other) {
+       this.x = other.x;
+       this.y = other.y;
+       this.heading = other.heading;
+    }
+
+    public void forward(double meters) {
+       x = x + meters * Math.cos(this.heading);
+       y = y + meters * Math.sin(this.heading);
+    }
+
+    public void rotate(double radians) {
+       this.heading = World.angleSum(this.heading, radians);
+    }
+    
+    public void rotate(float degrees) {
+       rotate(Math.toRadians(degrees));
+    }
+   @Override
+   public boolean equals(Object obj) {
+      if (obj == null) {
+         return false;
+      }
+      if (getClass() != obj.getClass()) {
+         return false;
+      }
+      final Pose other = (Pose) obj;
+      if (Double.doubleToLongBits(this.x) != Double.doubleToLongBits(other.x)) {
+         return false;
+      }
+      if (Double.doubleToLongBits(this.y) != Double.doubleToLongBits(other.y)) {
+         return false;
+      }
+      if (Double.doubleToLongBits(this.heading) != Double.doubleToLongBits(other.heading)) {
+         return false;
+      }
+      return true;
+   }
+
+   @Override
+   public int hashCode() {
+      int hash = 5;
+      hash = 97 * hash + (int) (Double.doubleToLongBits(this.x) ^ (Double.doubleToLongBits(this.x) >>> 32));
+      hash = 97 * hash + (int) (Double.doubleToLongBits(this.y) ^ (Double.doubleToLongBits(this.y) >>> 32));
+      hash = 97 * hash + (int) (Double.doubleToLongBits(this.heading) ^ (Double.doubleToLongBits(this.heading) >>> 32));
+      return hash;
+   }
 }
